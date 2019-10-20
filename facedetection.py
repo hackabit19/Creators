7f1fd84e2b18acfac0504df6129b1b10d3132bab
KratiAnu <47984303+KratiAnu@users.noreply.github.com>
2019-10-20 08:43:42 +0530

    Update facedetection.py

--- facedetection.py
+++ facedetection.py
@@ -1,28 +1,20 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jul 31 19:30:47 2019
-
-@author: ISM
-"""
-
 import cv2
 import numpy as np
 import serial
 
 face_cascade = cv2.CascadeClassifier("F://haarcascade_frontalface_default.xml")
-
-ser = serial.Serial('COM5',9600)
-focalLength = 3.85
-baseline = 60
-
+focalLength=3.85
+baseline=60
+ser=serial.Serial("com5",9600)
 #capturing the video images
 cap = cv2.VideoCapture(1)
 cap2 = cv2.VideoCapture(2)
 
 while True:
     ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)                #converting the image to gray_image
-    gray2 = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+    ret,img2 = cap2.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)#converting the image to gray_image
+    gray2=cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)         #using haarcascade classifier to detect the frontal face  
     for (x,y,w,h) in faces:                                     #making a rectangle around the frontal face
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)          
@@ -37,8 +29,11 @@ while True:
         cv2.putText(img, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
             
         #calculating the disparity map
-        stereo = cv2.StereoBM_create(numDisparities=128, blockSize=15)
+        stereo = cv2.StereoBM_create(numDisparities=128, blockSize=7)
         disparity = stereo.compute(gray,gray2)
+        
+        min = disparity.min()               
+        max = disparity.max()
         
         #Calculating the depth map using disparity
         #converting disparity into mm 
@@ -52,7 +47,7 @@ while True:
         print(-depth[cX,cY])
         ser.write(cX)
         ser.write(cY)
-        ser.write(depth)
+        ser.write(-depth[cX,cY])
     cv2.imshow('img' , img)
     if cv2.waitKey(30) & 0xFF == ord('q'):
         break
